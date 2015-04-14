3d242f0294acebdb1a3600002ce5a00f2358ac7e
Diego <diegoahg@users.noreply.github.com>
2015-04-14 19:33:40 -0300

    actualizacion codigo secuencial
    lee archivo, asigna voto y muestra rut y voto.

--- paralela.py
+++ paralela.py
@@ -1,40 +1,48 @@
 import random
+from openpyxl import load_workbook
 
-#################################
-#Funcione de Simulacion del Voto#
-#Retorna 1 si es IZQUERDA       #
-#Retorna 2 si es DERECHA        #
-#Retorna 3 si es INDEPENDIENTE  #
-#################################
-def Voto(izquierda , derecha , independiente):
-	rango1 = izquierda
-	rango2 = izquierda + derecha
-	probabilidad = random.randint(0, 100)
-	voto = 0
-	if probabilidad <= rango1:
-		voto = 1
-	elif probabilidad > rango1 and probabilidad <= rango2:
-		voto = 2
-	elif probabilidad > rango2 and probabilidad <= 100:
-		voto = 3
-	return voto
 
-voto = Voto(20,30,50)
-#print "El voto fue :" + str(voto)
+def Voto(izquierda, derecha, independiente):
+    rango1 = izquierda
+    rango2 = izquierda + derecha
+    probabilidad = random.randint(0, 100)
+    voto = 0
+    if probabilidad <= rango1:
+        voto = 1
+    elif probabilidad > rango1 and probabilidad <= rango2:
+        voto = 2
+    elif probabilidad > rango2 and probabilidad <= 100:
+        voto = 3
+    return voto
 
-comunas = [ ["Santiago", 20, 30, 40], ["Macul", 25,40,35]]
 
-#for i in range(len(comunas)):
-#    print comunas[i][0]
+def LeerComunas():
+    wb = load_workbook('comunas.xlsx')
+    ws = wb.get_sheet_by_name('Hoja1')
+    lista = []
+    for i in range(2, 349):
+        comuna = ws.cell('A' + str(i))
+        region = ws.cell('B' + str(i))
+        derecha = ws.cell('C' + str(i))
+        izquierda = ws.cell('D' + str(i))
+        independiente = ws.cell('E' + str(i))
+        dato = [comuna.value, region.value, derecha.value,
+                izquierda.value, independiente.value]
+        lista.append(dato)
+    return lista
 
-f = open("datos.csv")
-dato = f.readline()
-while dato != "":
-	datos = dato.split(';')
-	#print datos[1]
-	for i in range(len(comunas)):
-    		if datos[1] == comunas[i][0]:
-			 voto = Voto(comunas[i][1],comunas[i][2],comunas[i][3])
-			 print (datos[0] + ";" + datos[1] + ";" + datos[2] + ";" + str(voto) + ";")
-	dato = f.readline()
-f.close()
+
+def contarVoto(comunas):
+    wb = load_workbook('data_servel.xlsx')
+    ws = wb.get_sheet_by_name('Hoja1')
+    for i in range(2, 200002):
+        rut = ws.cell('A' + str(i))
+        rut = str(rut.value)
+        comuna = ws.cell('B' + str(i))
+        for j in range(len(comunas)):
+            if str(comuna.value) == comunas[j][0]:
+                voto = Voto(comunas[j][2], comunas[j][3], comunas[j][4])
+                print("rut: " + rut + "________________   voto: " + str(voto))
+
+comunas = LeerComunas()
+contarVoto(comunas)
