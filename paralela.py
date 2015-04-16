3ad8e620f40a71a30930952cc660f04fce54a86d
Robertoide <yo.soy.roberto@live.cl>
2015-04-16 13:16:10 -0300

    Codigo con CSV
    Abre todos los archivos en la carpeta!! (sate_servel_x.csv)

--- paralela.py
+++ paralela.py
@@ -31,31 +31,33 @@ def LeerComunas():
 
 
 def contarVoto(comunas):
-    reader = csv.reader(open('data_servel.csv', 'rb'))
     suma = 0
     sumb = 0
     sumc = 0
-    rango1 = 0
-    rango2 = 0
-    for i, row in enumerate(reader):
-        comuna = row[1]
-        for j in range(len(comunas)):
-            if comuna == comunas[j][0]:
-                rango1 = int(comunas[j][2])
-                rango2 = (int(comunas[j][2]) + int(comunas[j][3]))
-                voto = Voto(rango1, rango2)
-                if voto == 1:
-                    suma = suma + 1
-                elif voto == 2:
-                    sumb = sumb + 1
-                else:
-                    sumc = sumc + 1
+    for x in xrange(1, 4):
+        archivo = 'data_servel_' + str(x) + '.csv'
+        reader = csv.reader(open(archivo, 'rb'))
+        for i, row in enumerate(reader):
+            comuna = row[1]
+            for j in range(len(comunas)):
+                if comuna == comunas[j][0]:
+                    rango1 = int(comunas[j][2])
+                    rango2 = (int(comunas[j][2]) + int(comunas[j][3]))
+                    voto = Voto(rango1, rango2)
+                    if voto == 1:
+                        suma = suma + 1
+                    elif voto == 2:
+                        sumb = sumb + 1
+                    else:
+                        sumc = sumc + 1
+    sumtotal = suma + sumb + sumc
     print " izquierda: " + str(suma)
     print " derecha: " + str(sumb)
     print " independiente: " + str(sumc)
+    print " votos totales: " + str(sumtotal)
 
 tiempo_inicial = time()
 comunas = LeerComunas()
 contarVoto(comunas)
 tiempo_final = time() - tiempo_inicial
-print(tiempo_final)
+print "tiempo total de ejecucion: " + str(tiempo_final)
