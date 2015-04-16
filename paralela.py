45bced41666a64f15d33c33a5b03437b0c02ddae
Robertoide <yo.soy.roberto@live.cl>
2015-04-16 13:14:56 -0300

    Primera Version con CSV
    cambio de tipo de fichero.

--- paralela.py
+++ paralela.py
@@ -1,59 +1,61 @@
+import csv
 import random
-from openpyxl import load_workbook
+from time import time
 
 
-def Voto(izquierda, derecha, independiente):
-    rango1 = izquierda
-    rango2 = izquierda + derecha
-    probabilidad = random.randint(0, 100)
+def Voto(rango1, rango2):
+    probabilidad = random.randint(1, 100)
     voto = 0
     if probabilidad <= rango1:
         voto = 1
     elif probabilidad > rango1 and probabilidad <= rango2:
         voto = 2
-    elif probabilidad > rango2 and probabilidad <= 100:
+    else:
         voto = 3
     return voto
 
 
 def LeerComunas():
-    wb = load_workbook('comunas.xlsx')
-    ws = wb.get_sheet_by_name('Hoja1')
+    reader = csv.reader(open('comunas.csv', 'rb'))
     lista = []
-    for i in range(2, 349):
-        comuna = ws.cell('A' + str(i))
-        region = ws.cell('B' + str(i))
-        derecha = ws.cell('C' + str(i))
-        izquierda = ws.cell('D' + str(i))
-        independiente = ws.cell('E' + str(i))
-        dato = [comuna.value, region.value, derecha.value,
-                izquierda.value, independiente.value]
+    for i, row in enumerate(reader):
+        comuna = row[0]
+        region = row[1]
+        derecha = row[2]
+        izquierda = row[3]
+        independiente = row[4]
+        dato = [comuna, region, derecha,
+                izquierda, independiente]
         lista.append(dato)
     return lista
 
 
 def contarVoto(comunas):
-    wb = load_workbook('data_servel.xlsx')
-    ws = wb.get_sheet_by_name('Hoja1')
+    reader = csv.reader(open('data_servel.csv', 'rb'))
     suma = 0
     sumb = 0
     sumc = 0
-    for i in range(2, 200002):
-        rut = ws.cell('A' + str(i))
-        rut = str(rut.value)
-        comuna = ws.cell('B' + str(i))
+    rango1 = 0
+    rango2 = 0
+    for i, row in enumerate(reader):
+        comuna = row[1]
         for j in range(len(comunas)):
-            if str(comuna.value) == comunas[j][0]:
-                voto = Voto(comunas[j][2], comunas[j][3], comunas[j][4])
+            if comuna == comunas[j][0]:
+                rango1 = int(comunas[j][2])
+                rango2 = (int(comunas[j][2]) + int(comunas[j][3]))
+                voto = Voto(rango1, rango2)
                 if voto == 1:
                     suma = suma + 1
                 elif voto == 2:
                     sumb = sumb + 1
-                elif voto == 3:
+                else:
                     sumc = sumc + 1
-    print "izquierda: " + str(suma)
-    print "derecha: " + str(sumb)
-    print "independiente: " + str(sumc)
+    print " izquierda: " + str(suma)
+    print " derecha: " + str(sumb)
+    print " independiente: " + str(sumc)
 
+tiempo_inicial = time()
 comunas = LeerComunas()
 contarVoto(comunas)
+tiempo_final = time() - tiempo_inicial
+print(tiempo_final)
